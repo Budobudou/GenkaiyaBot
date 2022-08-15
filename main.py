40bfc098a3f2376d1f285d57e05a6a2043218be9
ぶどぶどう <63654313+Budobudou@users.noreply.github.com>
2022-08-15 22:55:22 +0900

    help コマンドを追加、細かい部分の修正

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import discord
+import discord,sys
 
 client = discord.Client()
-Token = "Your Token"
-
+Token = "your token"
+Version = "1.0 (Developer Preview)"
+Genkaiya_emoji = "<:genkaiya:1008703726405562474>"
 @client.event
 async def on_ready():
-    print("Login successful!")
+    print("起動しました")
     await client.change_presence(activity=discord.Game(name="限界やちゃん"))
 
 @client.event
@@ -13,14 +14,17 @@ async def on_message(message):
     if message.author.bot:
         return
     if '限界' in message.content:
-        await message.add_reaction("<genkaiya:1003377706521600042>")
-    elif message.content == '!ping':
+        await message.add_reaction("<:genkaiya:1008703726405562474>")
+    elif message.content == 'gen!ping':
         raw_ping = client.latency
         ping = round(raw_ping * 1000)
         await message.reply("Pong!\nBotのPing値は" + str(ping) + "msです。")
-    elif message.content == '!get':
-        await message.reply("Get Genkaiya!: https://github.com/brain-hackers/README/blob/main/assets.md")
-    elif message.content == '!license':
-        await message.reply("限界やちゃんは Brain-Hackers により、Creative Commons BY-SA 4.0 でライセンスされています。\n")
-
-client.run("Token")
+    elif message.content == 'gen!license':
+        await message.reply("限界やちゃんは Brain-Hackers により、Creative Commons BY-SA 4.0 でライセンスされています。\nhttps://github.com/brain-hackers/README/blob/main/assets.md")
+    elif message.content == 'gen!exit':
+        await message.channel.send("さよならー")
+        sys.exit()
+    elif message.content == 'gen!help':
+        helpcmd = f"限界やBot コマンドリスト\ngen!help 今実行したコマンドや...\ngen!ping Pingを測るコマンドや...\ngen!license ライセンス情報を表示するや...\n\nバージョン情報:{Version}"
+        await message.channel.send(helpcmd)
+client.run(Token)
