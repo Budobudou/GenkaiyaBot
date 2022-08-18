3a7e322108216ec9b92f01bfee387bffeff966b8
ぶどぶどう <63654313+Budobudou@users.noreply.github.com>
2022-08-18 15:10:57 +0900

    debug7

--- main.py
+++ main.py
@@ -39,9 +39,7 @@ async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
     print(now)
-    serversuu = len(client.guilds)
-    usersuu = len(client.users)
-    
+
     if now == '22:00':
         ch_name = "限界や出現数"
         global gencount
@@ -60,6 +58,8 @@ loop.start()
 @client.event
 async def on_ready():
     print("起動しました")
+    serversuu = len(client.guilds)
+    usersuu = len(client.users)
     await client.change_presence(activity=discord.Game(name=random.choice(("限界リアクション",f"現在、{serversuu}サーバーにいるや...",f"{usersuu}人と戯れてるや...","コマンド一覧の表示はgen!helpを入力してや...","gen!randomと打ってみてや...","「限界や」と言ってみてや..."))
 ))
     notify = await client.fetch_channel(startnotify_channel)
