d5868f650906fc37c301dc04e249919b8594a1ff
ぶどぶどう <63654313+Budobudou@users.noreply.github.com>
2022-08-23 22:39:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -210,12 +210,10 @@ async def on_message(message):
         except:pass
         for channel in global_channels:
             if channel.id == message.channel.id:continue
-            try:
-                ch_webhooks = await channel.webhooks()
-                if ch_webhooks == []:
-            except:continue
-                    try:webhook = await channel.create_webhook(name=GLOBAL_WEBHOOK_NAME, reason=f"{GLOBAL_CH_NAME}の為にwebhook作成したや...")
-                    except:continue
+            ch_webhooks = await channel.webhooks()
+            if ch_webhooks == []:
+                try:webhook = await channel.create_webhook(name=GLOBAL_WEBHOOK_NAME, reason=f"{GLOBAL_CH_NAME}の為にwebhook作成したや...")
+                except:continue
             else:
                 webhook = ch_webhooks[0]
             content = message.content.replace("@", "＠")
