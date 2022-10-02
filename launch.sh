dd8b32800c2c0d751ac198d99af1161e848e702d
ぶどぶどう <63654313+Budobudou@users.noreply.github.com>
2022-10-02 15:19:21 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
-
-nohup sh -c 'python3 slash.py; python3 rest.py' &
+echo GenkaiyaBot_bootloader
+nohup sh -c 'python3 slash.py' &
+nohup sh -c 'python3 rest.py' &
 source env/bin/activate
 nohup sh -c 'python3 reaction.py' &
